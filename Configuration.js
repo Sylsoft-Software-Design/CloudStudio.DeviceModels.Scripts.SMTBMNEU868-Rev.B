6cee9f979b6adf419f460fa07033d3a6d78c4868
Cristian Zangaro <cristian.zangaro@cloud.studio>
2025-04-16 17:51:07 -0300

    Initial commit
    Co-authored-by: Cristian Miguel Zangaro <cristian.zangaro@outlook.com>
    Co-authored-by: DarkSylver <darksylvergs@hotmail.com>

---
+++ Configuration.js
@@ -0,0 +1,70 @@
+function getConfiguration(config) {
+    // This function allows you to indicate general configuration values
+    // for all devices of this model.
+
+    // Depending on the meaning of the "device address" in this device, 
+    // you may want to change the label that is used to refer to the 
+    // address in the user interface.
+    // For instance, if the address of the device is actually a MAC 
+    // address, you may want to use the code below.
+
+    config.addressLabel = { en: "DevEUI", es: "DevEUI" };
+}
+
+function getEndpoints(deviceAddress, endpoints) {
+
+    endpoints.addEndpoint("1", "Temperature", endpointType.temperatureSensor);
+    endpoints.addEndpoint("2", "Humidity", endpointType.humiditySensor);
+    endpoints.addEndpoint("3", "Light", endpointType.lightSensor);
+    endpoints.addEndpoint("4", "Pressure", endpointType.pressureSensor);
+    endpoints.addEndpoint("5", "CO2", endpointType.ppmConcentrationSensor, ppmConcentrationSensorSubType.carbonDioxide);
+    endpoints.addEndpoint("6", "PIR", endpointType.iasSensor);
+
+}
+
+function validateDeviceAddress(address, result)
+{
+  // This function allows you to validate the address of a device, when
+  // the user is creating it. If your device has special validation rules
+  // for the address, you can check them here, and return an error message
+  // in case the address format is incorrect.
+
+  // In the code below, a validation is made to ensure that the address 
+  // is exactly 10 characters long.
+  
+  // if (address.length != 10) {
+  //   result.ok = false;
+  //   result.errorMessage = {
+  //     en: "The address must be 10 characters long", 
+  //     es: "La dirección debe tener exactamente 10 caracteres"
+  //   };
+  // }
+}
+
+function updateDeviceUIRules(device, rules)
+{
+  // This function allows you to specify UI rules at the device level.
+  // For instance, you can make it possible to enable or disable adding
+  // endpoints manually to the device after it was created.
+  
+  // In the code below, adding endpoints manually is disabled in the
+  // user interface. This means that the device will be limited to the 
+  // endpoints defined by function getEndpoints() above.
+  
+  // rules.canCreateEndpoints = false;
+}
+
+function updateEndpointUIRules(endpoint, rules)
+{
+  // This function allows you to specify UI rules at the endpoint level.
+  // For instance, you can make it possible to delete certain endpoints, 
+  // or edit their endpoint subtype, if applicable.
+
+  // In the code below, the following rules are defined:
+  // - Endpoints cannot be deleted.
+  // - The endpoint subtype can be changed, but only for the second 
+  //   endpoint.
+  
+  // rules.canDelete = false;
+  // rules.canEditSubType = (endpoint.address == "2");
+}
